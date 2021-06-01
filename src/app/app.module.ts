85cc4a47507780b7e68045a030d95b85581ed234
mbrito <mbrito@walook.com.mx>
2021-05-31 21:30:41 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { LandingComponent } from './landing/landing.component';
+import { VisorComponent } from './visor/visor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    LandingComponent,
+    VisorComponent
   ],
   imports: [
     BrowserModule,

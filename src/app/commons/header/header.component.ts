62218a3e9779240c6b61c5c3294c139cfaad07d5
Hilda Cruz <hcruz@walook.com.mx>
2021-06-14 00:34:25 -0500

    logout modal

--- src/app/commons/header/header.component.ts
+++ src/app/commons/header/header.component.ts
@@ -1,10 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
+    encapsulation: ViewEncapsulation.None,
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
     public isMenuCollapsed = true;
+    constructor(private modalService: NgbModal) {}
+    openMenuModal(content: any): void {
+        this.modalService.open(content, {backdropClass: 'color-backdrop'});
+    }
 }
